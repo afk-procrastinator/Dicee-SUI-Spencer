bcc487223110ac6f6f17034236861e57d2587f58
Spencer <spencermichaels1@gmail.com>
2019-11-04 17:08:01 -0800

    Initial Commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Spencer Michaels on 11/4/19.
+//  Copyright © 2019 Spencer Michaels. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
